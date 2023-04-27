82e645a2eda17ce17cba3e5e47f641c30ef94a32
thiagomb13 <thiago.muller@live.com>
2023-04-27 08:52:27 -0300

    Exercicio

---
+++ index.js
@@ -0,0 +1,23 @@
+//console.log("hello")
+// comentario
+// prompt("Qual é a sua idade?")
+// const nome = prompt("Qual é o seu nome?")
+// console.log(nome)
+// const idade = prompt("Qual é a sua idade?")
+// console.log(idade)
+// const endereco = prompt("Qual é o seu endereço")
+// console.log(endereco)
+
+// let anoNascimento = 1996
+// anoNascimento = 2023 - anoNascimento
+// console.log(anoNascimento)
+// const nome = prompt ("Qual é o seu nome?")
+// const idade = prompt ("Qual é a sua idade?")
+
+// console.log("Olá!", "Meu nome é" , nome, "e eu tenho", idade, "anos.")
+
+const nome = prompt ("Qual seu nome?")
+const sobrenome = prompt ("Qual seu sobrenome?")
+const idade = prompt ("Qual sua idade?")
+const escolaridade = prompt ("Você é estudante? (sim/não)")
+console.log("Nome:", nome, sobrenome, "Idade:", idade, "Estudante?", escolaridade);
